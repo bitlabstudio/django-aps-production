0c3b585a79ac378331bd37a7ee131b78d4603098
Daniel Kaufhold <daniel.kaufhold@bitmazk.com>
2014-11-27 15:11:48 +0100

    Added admins

--- aps_production/admin.py
+++ aps_production/admin.py
@@ -1,13 +1,47 @@
 """Admin classes for the aps_production app."""
-# from django.contrib import admin
+from django.contrib import admin
 
-# from . import models
+from . import models
 
 
-# class YourModelAdmin(admin.ModelAdmin):
-#    list_display = ['some', 'fields', ]
-#    search_fields = ['some', 'fieds', ]
+class ErrorAdmin(admin.ModelAdmin):
+    """Custom admin for the ``Error`` model."""
+    list_display = ('order_run', 'error_bin', 'quantity', 'comment')
 
-# admin.site.register(models.YourModel, YourModelAdmin)
 
-# TODO also add admins for the new aps bom stuff )m
+class ErrorBinAdmin(admin.ModelAdmin):
+    """Custom admin for the ``ErrorBin`` model."""
+    list_display = ('technology', 'error_code')
+
+
+class OrderAdmin(admin.ModelAdmin):
+    """Custom admin for the ``Order`` model."""
+    list_display = ('order_number', 'company', 'date_created',
+                    'customer_po_number', 'customer_po_date')
+
+
+class OrderLineAdmin(admin.ModelAdmin):
+    """Custom admin for the ``OrderLine`` model."""
+    list_display = ('order', 'line_no', 'product', 'quantity_ordered',
+                    'date_requested')
+
+
+class OrderRunAdmin(admin.ModelAdmin):
+    """Custom admin for the ```` model."""
+    list_display = ('order_line', 'run_number', 'parent', 'ipn',
+                    'quantity_started', 'quantity_dest_out', 'quantity_out',
+                    'is_open', 'comment')
+    list_filter = ['is_open']
+
+
+class ShipmentAdmin(admin.ModelAdmin):
+    """Custom admin for the ``Shipment`` model."""
+    list_display = ('order_run', 'quantity', 'date_shipped')
+
+
+admin.site.register(models.Error, ErrorAdmin)
+admin.site.register(models.ErrorBin, ErrorBinAdmin)
+admin.site.register(models.Order, OrderAdmin)
+admin.site.register(models.OrderLine, OrderLineAdmin)
+admin.site.register(models.OrderRun, OrderRunAdmin)
+admin.site.register(models.Shipment, ShipmentAdmin)
